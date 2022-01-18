0c08539d5f0e3e8cfdce7d4711913a58cacd4e37
Maksim Kuzmin <mkuzmin@luxoft.com>
2022-01-18 11:27:03 +0300

    Eject

---
+++ index.js
@@ -0,0 +1,15 @@
+// Copyright 2022 Russian Post
+// This source code is Russian Post Confidential Proprietary.
+// This software is protected by copyright. All rights and titles are reserved.
+// You shall not use, copy, distribute, modify, decompile, disassemble or reverse engineer the software.
+// Otherwise this violation would be treated by law and would be subject to legal prosecution.
+// Legal use of the software provides receipt of a license from the right holder only.
+
+import { registerRootComponent } from 'expo';
+
+import App from './App';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
